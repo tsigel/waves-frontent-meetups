0044be6352c78b9446d30eb5e87ae1cdd0d93d90
tsigel <tsddaniil@gmail.com>
2019-05-16 11:00:53 +0300

    Add next meetup template

---
+++ 2019-05-17/Signal.spec.ts
@@ -0,0 +1,90 @@
+class Signal<T> {
+    public on(callback: (data: T) => any): void {
+        return void 0;
+    }
+
+    public once(callback: (data: T) => any): void {
+        return void 0;
+    }
+
+    public off(callback?: (data: T) => any): void {
+        return void 0;
+    }
+
+    public dispatch(data: T): void {
+        return void 0;
+    }
+}
+
+
+describe('Signal', () => {
+
+    let signal: Signal<number> = new Signal();
+
+    it('create', () => {
+        expect(signal instanceof Signal).toBe(true);
+    });
+
+    it('on', () => {
+
+        let ok = false;
+        signal.on(() => {
+            ok = true;
+        });
+        signal.dispatch(null);
+        expect(ok).toBe(true);
+
+    });
+
+    it('once', () => {
+
+        let ok = false;
+        let count = 0;
+        signal.once(() => {
+            count++;
+            ok = count === 1;
+        });
+        signal.dispatch(null);
+        signal.dispatch(null);
+        expect(ok).toBe(true);
+
+    });
+
+    describe('off', () => {
+
+        it('by handler', () => {
+
+            let ok = false;
+            let count = 0;
+            let h1 = () => {
+                count++;
+                ok = count === 1;
+            };
+            let h2 = () => {
+                count++;
+            };
+            signal.on(h1);
+            signal.on(h2);
+            signal.on(h2);
+            signal.dispatch(null);
+            signal.off(h1);
+            signal.dispatch(null);
+            signal.off(h2);
+            expect(ok).toBe(true);
+            expect(count).toBe(5);
+
+        });
+
+    });
+
+    it('dispatch', () => {
+
+        let ok = false;
+        signal.on((data: number) => {
+            ok = data === 1;
+        });
+        signal.dispatch(1);
+        expect(ok).toBe(true);
+    });
+
+});
